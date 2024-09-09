1a9d8e6975e5f9f920ee8b2b4467fba9fe2eb22e
AWinRar <screw_reality@hotmail.com>
2024-09-10 02:02:50 +1000

    Update armorData.js
    Added more sets

--- armorData.js
+++ armorData.js
@@ -56,6 +56,230 @@ const armorData = {
 		weight: 25,
 		isMetal: true
 	},
+	"mithral Chain Shirt": {
+		proficiencyType: "Light",
+		cost: 110000,
+		acBonus: 4,
+		maxDexBonus: 6,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 12.5,
+		isMetal: true
+	},
+	"mithral Chainmail": {
+		proficiencyType: "Light",
+		cost: 415000,
+		acBonus: 5,
+		maxDexBonus: 4,
+		armorCheckPenalty: -2,
+		arcaneSpellFailureChance: 20,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 20,
+		isMetal: true
+	},
+	"Twisted Silk": {
+		proficiencyType: "Light",
+		cost: 40000,
+		acBonus: 3,
+		maxDexBonus: 7,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: true
+	},
+	"Bondleaf Wrap": {
+		proficiencyType: "Light",
+		cost: 100000,
+		acBonus: 1,
+		maxDexBonus: null,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 2,
+		isMetal: False
+	},
+	"Bone": {
+		proficiencyType: "Light",
+		cost: 2000,
+		acBonus: 3,
+		maxDexBonus: 4,
+		armorCheckPenalty: -3,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 20,
+		isMetal: False
+	},
+	"Chameleon Leather": {
+		proficiencyType: "Light",
+		cost: 36000,
+		acBonus: 2,
+		maxDexBonus: 6,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: False
+	},
+	"Leafweave Leather": {
+		proficiencyType: "Light",
+		cost: 75000,
+		acBonus: 2,
+		maxDexBonus: 7,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 9,
+		isMetal: False
+	},
+	"Leafweave Padded": {
+		proficiencyType: "Light",
+		cost: 74500,
+		acBonus: 7,
+		maxDexBonus: 9,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 8,
+		isMetal: False
+	},
+	"Leafweave Studded Leather": {
+		proficiencyType: "Light",
+		cost: 76500,
+		acBonus: 3,
+		maxDexBonus: 6,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: False
+	},
+	"Leather Scale": {
+		proficiencyType: "Light",
+		cost: 3500,
+		acBonus: 3,
+		maxDexBonus: 6,
+		armorCheckPenalty: -2,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 20,
+		isMetal: False
+	},
+	"mithral Breastplate": {
+		proficiencyType: "Light",
+		cost: 420000,
+		acBonus: 5,
+		maxDexBonus: 5,
+		armorCheckPenalty: -2,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: True
+	},
+	"Mithral Tumblers Breastplate": {
+		proficiencyType: "Medium",
+		cost: 445000,
+		acBonus: 5,
+		maxDexBonus: 6,
+		armorCheckPenalty: -2,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 15,
+		isMetal: true
+	},
+	"mithral Scale Mail": {
+		proficiencyType: "Light",
+		cost: 405000,
+		acBonus: 4,
+		maxDexBonus: 5,
+		armorCheckPenalty: -2,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: True
+	},
+	"Nightscale": {
+		proficiencyType: "Light",
+		cost: 100000,
+		acBonus: 2,
+		maxDexBonus: 10,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 3,
+		isMetal: False
+	},
+	"Sharkskin": {
+		proficiencyType: "Light",
+		cost: 8500,
+		acBonus: 3,
+		maxDexBonus: 6,
+		armorCheckPenalty: -1,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: False
+	},
+	"Wood": {
+		proficiencyType: "Light",
+		cost: 1500,
+		acBonus: 3,
+		maxDexBonus: 4,
+		armorCheckPenalty: -3,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 30,
+			twentyFeet: 20
+		},
+		weight: 15,
+		isMetal: False
+	},
 	"Hide": {
 		proficiencyType: "Medium",
 		cost: 1500,
@@ -103,8 +327,162 @@ const armorData = {
 		cost: 20000,
 		acBonus: 5,
 		maxDexBonus: 3,
-		armorCheckPenalty: 4,
+		armorCheckPenalty: -4,
 		arcaneSpellFailureChance: 25,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 30,
+		isMetal: true
+	},
+	"Brigandine": {
+		proficiencyType: "Medium",
+		cost: 3000,
+		acBonus: 4,
+		maxDexBonus: 2,
+		armorCheckPenalty: -4,
+		arcaneSpellFailureChance: 30,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 40,
+		isMetal: true
+	},
+	"Lamellar": {
+		proficiencyType: "Medium",
+		cost: 15000,
+		acBonus: 5,
+		maxDexBonus: 3,
+		armorCheckPenalty: -4,
+		arcaneSpellFailureChance: 30,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 35,
+		isMetal: true
+	},
+	"Ring": {
+		proficiencyType: "Medium",
+		cost: 7500,
+		acBonus: 4,
+		maxDexBonus: 4,
+		armorCheckPenalty: -3,
+		arcaneSpellFailureChance: 30,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 35,
+		isMetal: true
+	},
+	"Chitin": {
+		proficiencyType: "Medium",
+		cost: 7500,
+		acBonus: 4,
+		maxDexBonus: 4,
+		armorCheckPenalty: -3,
+		arcaneSpellFailureChance: 30,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 20,
+		isMetal: False
+	},
+	"Interlocking Scale": {
+		proficiencyType: "Medium",
+		cost: 25000,
+		acBonus: 3,
+		maxDexBonus: 5,
+		armorCheckPenalty: -1,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 30,
+		isMetal: true
+	},
+	"Leafweave Hide": {
+		proficiencyType: "Medium",
+		cost: 75500,
+		acBonus: 3,
+		maxDexBonus: 5,
+		armorCheckPenalty: -1,
+		arcaneSpellFailureChance: 10,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 20,
+		isMetal: False
+	},
+	"Mithral Splint Mail": {
+		proficiencyType: "Medium",
+		cost: 920000,
+		acBonus: 6,
+		maxDexBonus: 2,
+		armorCheckPenalty: -5,
+		arcaneSpellFailureChance: 30,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 22.5,
+		isMetal: true
+	},
+	"Mithral Banded Mail": {
+		proficiencyType: "Medium",
+		cost: 925000,
+		acBonus: 6,
+		maxDexBonus: 3,
+		armorCheckPenalty: -4,
+		arcaneSpellFailureChance: 20,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 17.5,
+		isMetal: true
+	},
+	"Mithral Half-Plate":{
+		proficiencyType: "Medium",
+		cost: 960000,
+		acBonus: 7,
+		maxDexBonus: 2,
+		armorCheckPenalty: -5,
+		arcaneSpellFailureChance: 30,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 25,
+		isMetal: true
+	},
+	"Mithral Full Plate": {
+		proficiencyType: "Medium",
+		cost: 1050000,
+		acBonus: 8,
+		maxDexBonus: 3,
+		armorCheckPenalty: -4,
+		arcaneSpellFailureChance: 20,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 25,
+		isMetal: true
+	},
+	"Tumblers Breastplate": {
+		proficiencyType: "Medium",
+		cost: 45000,
+		acBonus: 5,
+		maxDexBonus: 4,
+		armorCheckPenalty: -4,
+		arcaneSpellFailureChance: 20,
 		speed: {
 			thirtyFeet: 20,
 			twentyFeet: 15
@@ -168,6 +546,48 @@ const armorData = {
 		weight: 50,
 		isMetal: true
 	},
+	"Battle Plate": {
+		proficiencyType: "Heavy",
+		cost: 250000,
+		acBonus: 9,
+		maxDexBonus: 1,
+		armorCheckPenalty: -7,
+		arcaneSpellFailureChance: 50,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 125,
+		isMetal: true
+	},
+	"Heavy Plate": {
+		proficiencyType: "Heavy",
+		cost: 200000,
+		acBonus: 9,
+		maxDexBonus: 0,
+		armorCheckPenalty: -8,
+		arcaneSpellFailureChance: 40,
+		speed: {
+			thirtyFeet: 20,
+			twentyFeet: 15
+		},
+		weight: 100,
+		isMetal: true
+	},
+	"Mountain Plate": {
+		proficiencyType: "Heavy",
+		cost: 325000,
+		acBonus: 10,
+		maxDexBonus: 0,
+		armorCheckPenalty: -9,
+		arcaneSpellFailureChance: 60,
+		speed: {
+			thirtyFeet: 15,
+			twentyFeet: 10
+		},
+		weight: 225,
+		isMetal: true
+	},
 	"Buckler": {
 		proficiencyType: "Shield",
 		cost: 1500,
@@ -180,6 +600,20 @@ const armorData = {
 			twentyFeet: null
 		},
 		weight: 5,
+		isMetal: true
+	},
+	"Mithral Buckler": {
+		proficiencyType: "Shield",
+		cost: 101500,
+		acBonus: 1,
+		maxDexBonus: null,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: null,
+			twentyFeet: null
+		},
+		weight: 2.5,
 		isMetal: true
 	},
 	"Light Wooden Shield": {
@@ -210,6 +644,20 @@ const armorData = {
 		weight: 6,
 		isMetal: true
 	},
+	"Mithral Light Steel Shield": {
+		proficiencyType: "Shield",
+		cost: 100900,
+		acBonus: 1,
+		maxDexBonus: null,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 0,
+		speed: {
+			thirtyFeet: null,
+			twentyFeet: null
+		},
+		weight: 3,
+		isMetal: true
+	},
 	"Heavy Wooden Shield": {
 		proficiencyType: "Shield",
 		cost: 700,
@@ -238,6 +686,20 @@ const armorData = {
 		weight: 15,
 		isMetal: true
 	},
+	"Mithral Heavy Steel Shield": {
+		proficiencyType: "Shield",
+		cost: 1020,
+		acBonus: 2,
+		maxDexBonus: null,
+		armorCheckPenalty: 0,
+		arcaneSpellFailureChance: 5,
+		speed: {
+			thirtyFeet: null,
+			twentyFeet: null
+		},
+		weight: 7.5,
+		isMetal: true
+	},
 	"Tower Shield": {
 		proficiencyType: "Tower Shield",
 		cost: 3000,
@@ -250,6 +712,20 @@ const armorData = {
 			twentyFeet: null
 		},
 		weight: 45,
+		isMetal: true
+	},
+	"Mithral Tower Shield": {
+		proficiencyType: "Tower Shield",
+		cost: 103000,
+		acBonus: 4,
+		maxDexBonus: 4,
+		armorCheckPenalty: -7,
+		arcaneSpellFailureChance: 40,
+		speed: {
+			thirtyFeet: null,
+			twentyFeet: null
+		},
+		weight: 50,
 		isMetal: true
 	}
 
